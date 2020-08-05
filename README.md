239b61e4ccfa1cdaa40bc837d0e195b18be594f1
Yves <yves.dornier@yahoo.fr>
2020-08-05 19:59:47 +0200

    New beta function

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MyFirstProject
 My first project
+
+#### 2020-08-04
+New branch for beta function.
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 
 print("Ceci est juste un test")
+print("Une fonction beta a été ajouté")
